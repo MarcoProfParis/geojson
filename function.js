516935ac2482c89e6ff6066afd51ad35e71dcc1b
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2025-03-08 21:47:11 +0100

    Update function.js

--- function.js
+++ function.js
@@ -5,18 +5,9 @@ let obj = JSON.parse(json.value);
 if (obj.check === false) return "Waiting for check";
     let url = obj.fetchUrl;
     console.log("Value of obj.fetchUrl:", obj.fetchUrl);
-    const requestBody = json;
-
-    // Append the password as a query parameter to the fetchUrl URL
-    // fetchUrl += `?pwd=${encodeURIComponent(json.pwd)}`;
-    // Check if fetchUrl is a valid URL and if check is true
     
-
-
-    console.log("Value of fetchUrl:", fetchUrl.value);
     const requestOptions = {
-        method: 'POST',
-        body: JSON.stringify(requestBody),
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         },
